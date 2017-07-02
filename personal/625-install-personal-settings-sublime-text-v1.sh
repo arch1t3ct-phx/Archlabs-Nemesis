fed258752192064261186ed1f9e689559e070803
Erik Dubois <erik.dubois@gmail.com>
2017-07-02 12:50:44 +0200

    Comment : updated sublime-text on Sun Jul  2 12:50:44 CEST 2017

--- personal/625-install-personal-settings-sublime-text-v1.sh
+++ personal/625-install-personal-settings-sublime-text-v1.sh
@@ -13,10 +13,11 @@ set -e
 
 [ -d $HOME"/.config/sublime-text-3" ] || mkdir -p $HOME"/.config/sublime-text-3"
 [ -d $HOME"/.config/sublime-text-3/Packages" ] || mkdir -p $HOME"/.config/sublime-text-3/Packages"
+[ -d $HOME"/.config/sublime-text-3/Installed\ Packages" ] || mkdir -p $HOME"/.config/sublime-text-3/Installed Packages"
 [ -d $HOME"/.config/sublime-text-3/Packages/User" ] || mkdir -p $HOME"/.config/sublime-text-3/Packages/User"
 
-cp settings/sublimetext/Default.sublime-theme ~/.config/sublime-text-3/Packages/User/
-cp settings/sublimetext/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/
+cp -r settings/sublimetext/Packages/* ~/.config/sublime-text-3/Packages/
+cp -r settings/sublimetext/Installed\ Packages/* ~/.config/sublime-text-3/Installed\ Packages/
 
 echo "################################################################"
 echo "#########       sublime text settings copied    ################"
