3a100fd3710e0b39d22ab64c0e5aa7b96bce7b55
Erik Dubois <erik.dubois@gmail.com>
2017-07-04 20:24:47 +0200

    Comment : virtualbox update on Tue Jul  4 20:24:47 CEST 2017

--- install-virtualbox-for-kernel-4.11-v1.sh
+++ install-virtualbox-for-kernel-4.11-v1.sh
@@ -11,9 +11,7 @@
 #
 ##################################################################################################################
 
-sudo pacman -S --noconfirm --needed virtualbox-host-dkms
-sudo pacman -S --noconfirm --needed linux-headers
-sudo pacman -S --noconfirm --needed linux-lts-headers
+sudo pacman -S --noconfirm --needed virtualbox-host-modules-arch
 
 
 package="virtualbox"
@@ -66,59 +64,8 @@ else
 fi
 
 
-# Archlab has already virtualbox-host-dkms installed
-
-# package="virtualbox-host-modules-arch"
 
 
-# #----------------------------------------------------------------------------------
-
-# #checking if application is already installed or else install with aur helpers
-# if pacman -Qi $package &> /dev/null; then
-
-# 	echo "################################################################"
-# 	echo "################## "$package" is already installed"
-# 	echo "################################################################"
-
-# else
-
-# 	#checking which helper is installed
-# 	if pacman -Qi packer &> /dev/null; then
-
-# 		echo "Installing with packer"
-# 		echo "confirm installation of virtualbox-host-modules-arch"
-# 		packer -S --noedit  $package
-
-# 	elif pacman -Qi pacaur &> /dev/null; then
-		
-# 		echo "Installing with pacaur"
-# 		pacaur -S --noconfirm --noedit  $package
-		 	
-# 	elif pacman -Qi yaourt &> /dev/null; then
-
-# 		echo "Installing with yaourt"
-# 		yaourt -S --noconfirm $package
-			  	
-# 	fi
-
-# 	# Just checking if installation was successful
-# 	if pacman -Qi $package &> /dev/null; then
-	
-# 	echo "################################################################"
-# 	echo "#########  "$package" has been installed"
-# 	echo "################################################################"
-
-# 	else
-
-# 	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-# 	echo "!!!!!!!!!  "$package" has NOT been installed"
-# 	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-
-# 	fi
-
-# fi
-
-
-	echo "################################################################"
-	echo "#########           You got to reboot.                 #########" 
-	echo "################################################################"
+echo "################################################################"
+echo "#########           You got to reboot.                 #########" 
+echo "################################################################"
